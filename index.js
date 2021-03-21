6302f50a14a3da21340fce0be836ab76e1a3cef5
WalukaUI <cwaluka@yahoo.com>
2021-03-20 23:08:31 -0700

    success all tests

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+let main = document.querySelector('main');
+main.remove();
+
+let newHeader = document.getElementById("victory");
+newHeader.h1; 
+newHeader.id;
+newHeader.innerHTML="Waluka is the champion"
